e160cec3a1da77a32f6175d33cf5658d81eb0add
BishwoSubedi <bishwosubedu@example.com>
2025-11-17 11:34:02 +0000

    tHIRD commit

--- storage.py
+++ storage.py
@@ -1,21 +1,29 @@
-def save(bank, filename="data/bank_data.txt"):
+def save(bank, filename="bank_data.txt"):
     with open(filename, "w") as f:
         for acc_no, acc in bank.accounts.items():
-            f.write(f"{acc_no},{acc.get_balance()}\n")
+            line = f"{acc_no},{acc.get_balance()}\n"
+            f.write(line)
 
-def load(filename="data/bank_data.txt"):
-    from bank import Bank
+
+def load(filename="bank_data.txt"):
+    from bank import Bank  
+
     bank = Bank()
+
     try:
         with open(filename, "r") as f:
             for line in f:
                 acc_no, balance = line.strip().split(",")
                 balance = float(balance)
+
+                # Recreate account as SavingsAccount by default
                 bank.accounts[acc_no] = bank.create_account(
                     name="Restored User",
                     acc_type="savings",
                     balance=balance
                 )
+
         return bank
+
     except FileNotFoundError:
         return None
