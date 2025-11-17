7aeb59f84bccae0b3ba11a9a672d19cbf045834d
BishwoSubedi <bishwosubedu@example.com>
2025-11-17 11:29:09 +0000

    second commit

--- storage.py
+++ storage.py
@@ -1,13 +1,21 @@
-import pickle
+def save(bank, filename="data/bank_data.txt"):
+    with open(filename, "w") as f:
+        for acc_no, acc in bank.accounts.items():
+            f.write(f"{acc_no},{acc.get_balance()}\n")
 
-def save(bank, filename="bank_data.pkl"):
-    with open(filename, "wb") as f:
-        pickle.dump(bank, f)
-
-def load(filename="bank_data.pkl"):
+def load(filename="data/bank_data.txt"):
+    from bank import Bank
+    bank = Bank()
     try:
-        with open(filename, "rb") as f:
-            bank = pickle.load(f)
+        with open(filename, "r") as f:
+            for line in f:
+                acc_no, balance = line.strip().split(",")
+                balance = float(balance)
+                bank.accounts[acc_no] = bank.create_account(
+                    name="Restored User",
+                    acc_type="savings",
+                    balance=balance
+                )
         return bank
     except FileNotFoundError:
         return None
